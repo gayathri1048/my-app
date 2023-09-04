6fdb8512d661a50f939e73f68df079482063faac
gayathri1048 <gayathrimuthumula@gmail.com>
2023-09-04 11:39:44 +0530

    commit

---
+++ src/components/Technology.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Technology=()=>{
+    return (
+        <div>Technology</div>
+    )
+}
+export default Technology
---
+++ src/components/website.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Website=()=>{
+    return (
+        <div>Website</div>
+    )
+}
+export default Website
